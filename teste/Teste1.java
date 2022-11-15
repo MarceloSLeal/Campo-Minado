d4dc617cff55db0e9a57279ebea6ee4da03a0de5
MarceloSLeal <marcelosleal@outlook.com>
2022-11-15 02:36:53 -0300

    Primeiro exemplo de teste unitário com JUnit e primeiro commit do repositório

---
+++ teste/Teste1.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Teste1Test {
+
+    //
+    @Test
+    void testar() {
+        int a = 0;
+
+        assertEquals(0, a);
+    }
+}
